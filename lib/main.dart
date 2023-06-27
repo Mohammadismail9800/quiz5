abbcc8a1d79006182300a0f5d5ff9dc07b885061
gdu29 <gdu29@banoqabil.pk>
2023-06-27 22:06:09 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body:Container(
+          child: Image.network(src),
+          child: leading: Icon(Icons.add_alert_rounded),
+        child: actions: [Icon(Icons.add_shopping_cart)],
+
+        )
+          child: row(
+          childern:[
+         Text("OverView Review"),
+         Text("review"),
+         Text("5 hours"),
+         Text("abcd"),
+      ],
+
+        ElevatedButton(onPressed:(){
+
+      };
+      child: Text("book Now"),
+    ),
+    );
+  }
+}
